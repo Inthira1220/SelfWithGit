5729d9570d68def4f3d1cf54db6931e9e5588dc0
Inthira1220 <82923298+Inthira1220@users.noreply.github.com>
2021-05-24 01:20:09 -0400

    correct word
    correct word

--- src/test/java/HelloInthira.java
+++ src/test/java/HelloInthira.java
@@ -2,6 +2,6 @@ public class HelloInthira {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("first try from githubWeb");
-        System.out.println("2nd try from githubWeb");
+        System.out.println("2nd try from local repository");
     }
 }
